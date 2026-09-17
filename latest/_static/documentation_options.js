7efd89469b0225f4039ca840229acd0b9992b821
prjemian <prjemian@users.noreply.github.com>
2026-09-17 19:28:57 +0000

    deploy: 2a082d7304380eee8e5c9b325c6aefce6c889c70

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.0rc2.dev1+gffe59a5bf',
+    VERSION: '1.0.0rc2.dev2+g2a082d730',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
